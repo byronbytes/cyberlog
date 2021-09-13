ae79cc8c50b723100d0c9e1daaec97a1ffec2cd9
Byron <53088136+byronbytes@users.noreply.github.com>
2021-09-13 07:40:24 -0400

    Update OpenWindowsFolder.bat

--- 1 - Can I BREAK My School's Security?/OpenWindowsFolder.bat
+++ 1 - Can I BREAK My School's Security?/OpenWindowsFolder.bat
@@ -1,6 +1,7 @@
 :: .
+:: Severity Level: 0.5/5
 :: If this doesn't work since your school disallows downloads, open Notepad and create / save it as a .bat extension.
-:: If it still manages not to work, then we will find another way to break the system.
+:: If it still manages to not work, then we will find another way to break the system.
 :: .
 
 @echo off

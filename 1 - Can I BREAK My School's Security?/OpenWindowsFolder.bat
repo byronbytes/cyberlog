2c74e8a435e8d0f8fd863f07ae6897d637f78d6a
Byron <53088136+byronbytes@users.noreply.github.com>
2021-09-13 20:40:24 -0400

    Update OpenWindowsFolder.bat

--- 1 - Can I BREAK My School's Security?/OpenWindowsFolder.bat
+++ 1 - Can I BREAK My School's Security?/OpenWindowsFolder.bat
@@ -1,13 +1,10 @@
 :: .
-:: Severity Level: 0.5/5
+:: Severity Level: 0.7/5
 :: If this doesn't work since your school disallows downloads, open Notepad and create / save it as a .bat extension.
-:: If it still manages to not work, then we will find another way to break the system.
+:: If it still doesn't work, then this has probably been patched.
 :: .
 
 @echo off
-
 echo Scijack Vulnerabilities #1 - Opening a C: Drive Window
-
 explorer C:\Windows
-
 pause
